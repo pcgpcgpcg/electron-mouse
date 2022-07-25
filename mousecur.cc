9e6adb67fc95750ac531f5c2290af7091ebe539f
pcg <pcgpcg@outlook.com>
2022-07-25 15:30:42 +0800

    ShowCursor跟系统的名字冲突了

--- mousecur.cc
+++ mousecur.cc
@@ -1,55 +1,43 @@
 #include <napi.h>
+#define OEMRESOURCE
 #include <windows.h>
+#include <winuser.h>
 
 //loadCursorFromFile,
 //setSystemCursor
 
-Napi::Value SetNullCursor(const Napi::CallbackInfo& info) {
+
+Napi::Value ShowMouseCursor(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
 
-  // if (info.Length() < 2) {
-  //   Napi::TypeError::New(env, "Wrong number of arguments")
-  //       .ThrowAsJavaScriptException();
-  //   return env.Null();
-  // }
+  if (info.Length() < 1) {
+    Napi::TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
+    return env.Null();
+  }
 
-  // if (!info[0].IsString()) {
-  //   Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
-  //   return env.Null();
-  // }
+  if (!info[0].IsBoolean()) {
+    Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
 
-  // string arg0 = info[0].As<Napi::String>().StringValue();
-  HCURSOR hCurDef = CopyCursor(LoadCursor(0, IDC_ICON));
-  SetSystemCursor(hCurDef, OCR_NORMAL);
-  DestroyCursor(hCurDef);
-  return 0;
+
+  bool arg0 = info[0].As<Napi::Boolean>().Value();
+  ::ShowCursor(arg0);
+  Napi::Number num = Napi::Number::New(env, 0);
+  return num;
 }
 
-// Napi::Value SetCursorFromFile(const Napi::CallbackInfo& info) {
-//   Napi::Env env = info.Env();
-
-//   if (info.Length() < 1) {
-//     Napi::TypeError::New(env, "Wrong number of arguments")
-//         .ThrowAsJavaScriptException();
-//     return env.Null();
-//   }
-
-//   if (!info[0].IsString()) {
-//     Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
-//     return env.Null();
-//   }
-
-//   // string arg0 = info[0].As<Napi::String>().StringValue();
-//   HCURSOR hCurDef = CopyCursor(LoadCursorFromFile(TEXT("cursor.cur")));
-//   SetSystemCursor(hCurDef, OCR_NORMAL);
-//   DestroyCursor(hCurDef);
-//   return 0;
-// }
-
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set(Napi::String::New(env, "SetNullCursor"), Napi::Function::New(env, SetNullCursor));
+  exports.Set(Napi::String::New(env, "ShowMouseCursor"), Napi::Function::New(env, ShowMouseCursor));
   //exports.Set(Napi::String::New(env, "SetCursorFromFile"), Napi::Function::New(env, SetCursorFromFile));
   return exports;
 }
 
 NODE_API_MODULE(mousecur, Init)
+
+
+  // string arg0 = info[0].As<Napi::String>().StringValue();
+  //HCURSOR hCurDef = CopyCursor(LoadCursor(NULL, IDC_ARROW));
+  //SetSystemCursor(hCurDef, OCR_NORMAL);
+  //DestroyCursor(hCurDef);
--- mousecur.js
+++ mousecur.js
@@ -1,5 +1,7 @@
 var mousecur = require('bindings')('mousecur.node')
 
-mousecur.SetNullCursor();
+mousecur.ShowMouseCursor(false);
+
+console.log('after setNullCursor');
 
 //console.log('This should be eight:', addon.add(3, 5))
