c38cb185cdc72652eef418eb537aede918583c3b
pcg <pcgpcg@outlook.com>
2022-07-25 15:55:18 +0800

    add js export

--- mousecur.js
+++ mousecur.js
@@ -1,7 +1,13 @@
-var mousecur = require('bindings')('mousecur.node')
+//var mousecur = require('bindings')('mousecur.node')
 
-mousecur.ShowMouseCursor(false);
+// mousecur.ShowMouseCursor(false);
 
-console.log('after setNullCursor');
+// console.log('after setNullCursor');
 
 //console.log('This should be eight:', addon.add(3, 5))
+if (process.platform !== 'win32') {
+    throw new Error('Only works on windows.')
+  } else {
+    const mousecur = require('bindings')('mousecur.node')
+    module.exports = mousecur
+  }
